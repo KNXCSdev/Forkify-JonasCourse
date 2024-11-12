d856a9570741afa088fbc274dc783bae5c6e592f
KNXCSdev <michal.miksho@gmail.com>
2024-11-12 15:10:33 +0100

    Add input button

--- src/js/config.js
+++ src/js/config.js
@@ -5,4 +5,5 @@ export const RES_PER_PAGE = 16;
 export const KEY = "0ed09881-bd8d-4375-a85d-8178d2effa8a";
 export const SPOON_KEY = "544c747b89c44e1a9167fddea9e9557c";
 export const MODAL_CLOSE_SEC = 2.5;
+export const MAX_INGREDIENTS = 10;
 //
--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,5 +1,6 @@
 import View from "./View.js";
 import icons from "url:../../img/icons.svg"; // Parcel 2
+import { MAX_INGREDIENTS } from "../config.js";
 
 class AddRecipeView extends View {
   _parentElement = document.querySelector(".upload");
@@ -9,11 +10,13 @@ class AddRecipeView extends View {
   _overlay = document.querySelector(".overlay");
   _btnOpen = document.querySelector(".nav__btn--add-recipe");
   _btnClose = document.querySelector(".btn--close-modal");
+  _btnAdd = document.querySelector(".add__ingredient");
 
   constructor() {
     super();
     this._addHandlerShowWindow();
     this._addHandlerHideWindow();
+    this.addHandlerAddIngredient();
   }
 
   toggleWindow() {
@@ -28,6 +31,47 @@ class AddRecipeView extends View {
   _addHandlerHideWindow() {
     this._btnClose.addEventListener("click", this.toggleWindow.bind(this));
     this._overlay.addEventListener("click", this.toggleWindow.bind(this));
+  }
+
+  // Adding more ingredients input fields to add recipe functionality
+  _addNewIngredient() {
+    // count of existing ingredients
+    const countIng = Array.from(this._parentElement.querySelectorAll(".ingredients")).length;
+
+    // Hide '+' button, if limit of ingredients is passed
+    if (countIng + 1 === MAX_INGREDIENTS) this._btnAdd.classList.add("hidden");
+
+    return `
+          <label>Ingredient ${countIng + 1}</label>
+          <div class="ingredients">
+            <input value="" type="number" pattern="[0-9]*" required name="ingredient-${
+              countIng + 1
+            }_quantity" placeholder="Quantity" />
+            <select name="ingredient-${countIng + 1}_unit">
+              <option selected disabled hidden>Unit</option>
+              <option value="gram">Gram</option>
+              <option value="milligram">Milligram</option>
+              <option value="kilogram">Kilogram</option>
+              <option value="ounce">Ounce</option>
+              <option value="pound">Pound</option>
+              <option value="litre">Litre</option>
+              <option value="millilitre">Millilitre</option>
+            </select>
+            <input value="" type="text" pattern="[A-Za-z]*" required name="ingredient-${
+              countIng + 1
+            }_description" placeholder="Description" />
+          </div>
+    `;
+  }
+
+  // When '+' button is clicked
+  addHandlerAddIngredient() {
+    this._btnAdd.addEventListener(
+      "click",
+      function () {
+        this._btnAdd.insertAdjacentHTML("beforebegin", this._addNewIngredient());
+      }.bind(this)
+    );
   }
 
   addHandlerUpload(handler) {
