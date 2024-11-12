953c2f0b774d078c343d099be2b039b798e2e4bb
KNXCSdev <michal.miksho@gmail.com>
2024-11-12 14:55:16 +0100

    Changed input script

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -34,7 +34,9 @@ class AddRecipeView extends View {
     this._parentElement.addEventListener("submit", function (e) {
       e.preventDefault();
       const dataArr = [...new FormData(this)];
+
       const data = Object.fromEntries(dataArr);
+
       handler(data);
     });
   }
