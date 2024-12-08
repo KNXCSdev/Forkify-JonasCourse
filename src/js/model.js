6ef8b77a59040cef0471ff0042328b55c6148739
KNXCSdev <michal.miksho@gmail.com>
2024-12-08 12:24:24 +0100

    Final

--- src/js/model.js
+++ src/js/model.js
@@ -23,7 +23,7 @@ const createRecipeObject = function (data) {
     title: recipe.title,
     publisher: recipe.publisher,
     sourceUrl: recipe.source_url,
-    image: recipe.image_url,
+    imageUrl: recipe.image_url,
     servings: recipe.servings,
     cookingTime: recipe.cooking_time,
     ingredients: recipe.ingredients,
